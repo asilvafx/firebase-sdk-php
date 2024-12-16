21062fc6fa6ebee18b0e157e760d015816d8f753
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2024-12-16 09:09:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,86 +4,63 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/vendor/autoload.php';
+require_once('db_service.php'); 
+ 
+$db = new DBService(); 
 
-use Kreait\Firebase\Factory;
+// Example usage
+// Get table data
+/*
+    $tableData = $db->getData('users');
+    print_r($tableData);
+*/
 
-$projectId = 'YOUR_FIREBASE_PROJECT_ID';
-$databaseUrl = 'https://YOUR_FIREBASE_DB_URL.firebaseio.com/';
+// Get item data by Search Query
+/*
+    $itemData = $db->getItem('users', 'uid', 'XXXXXX');
+    print_r($itemData);
+*/
 
-// Replace with the path to your service account key file
-$keyFilePath = 'json/YOUR_SERVICE_ACCOUNT.json';
+// Post new user data
+/*
+    $postData = [
+    'avatar' => 'https://example.com/avatar.png',
+    'credits' => 100,
+    'displayName' => 'New User',
+    'email' => 'newuser@example.com',
+    'password' => 'hashed_password',
+    'status' => '',
+    'uid' => 'unique_user_id'
+    ];
+    $db->postData('users', $postData);
+*/
 
-$factory = (new Factory)
-    ->withServiceAccount($keyFilePath)
-    ->withDatabaseUri($databaseUrl);
+// Get item key by UID
+/*
+    $queryToFind = 'uid';
+    $searchToFind = 'bdd62134-3e25-47a4-859b-b70268504a0d';
+    $tableToFind = 'users';
 
-$database = $factory->createDatabase();
-
-/**
- * Get data from the database
- *
- * @param string $path The path to the data in the database
- * @return mixed The data retrieved from the database
- */
-function getData($database, $path) {
-    $reference = $database->getReference($path);
-    $snapshot = $reference->getSnapshot();
-    return $snapshot->getValue();
-}
-
-/**
- * Post new data to the database
- *
- * @param string $path The path to the data in the database
- * @param array $data The data to be posted
- * @return void
- */
-function postData($database, $path, $data) {
-    $reference = $database->getReference($path);
-    $reference->push($data);
-}
-
-/**
- * Update existing data in the database
- *
- * @param string $path The path to the data in the database
- * @param array $data The data to be updated
- * @return void
- */
-function updateData($database, $path, $data) {
-    $reference = $database->getReference($path);
-    $reference->update($data);
-}
-
-/**
- * Delete data from the database
- *
- * @param string $path The path to the data in the database
- * @return void
- */
-function deleteData($database, $path) {
-    $reference = $database->getReference($path);
-    $reference->remove();
-}
-
-/**
- * Get user key by UID
- *
- * @param object $database The database instance
- * @param string $uid The UID of the user
- * @return string|null The user key if found, null otherwise
- */
-function getUserKeyByUid($database, $uid) {
-$users = getData($database, 'users/');
-
-    if (is_array($users)) {
-        foreach ($users as $key => $user) {
-            if (isset($user['uid']) && $user['uid'] === $uid) {
-                return $key; // Return the user key
-            }
-        }
+    $userKey = $db->getKeyByQuery($tableToFind, $queryToFind, $searchToFind);
+    if ($userKey) {
+        echo "DB: $tableToFind | UID: $searchToFind | KEY: $userKey\n";
+    } else {
+        echo "DB: $tableToFind | UID: $searchToFind | KEY: Not found.\n";
     }
+*/
 
-    return null; // Return null if UID not found
-}
+// Update existing user data
+/*
+    $updateData = [
+    'credits' => 150
+    ];
+    $db->updateData('users' . $userKey, $updateData);
+*/
+
+// Delete user data
+/*
+    $db->deleteData('users' . $userKey);
+*/
+
+
+exit;
